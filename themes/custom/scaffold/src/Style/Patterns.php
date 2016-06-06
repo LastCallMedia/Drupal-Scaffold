156f9cddfa322f8d60bd0daf6d889b55b1f1eff2
Rob Bayliss <rob@lastcallmedia.com>
2016-06-06 14:48:33 -0400

    Fill out basic patterns

--- themes/custom/scaffold/src/Style/Patterns.php
+++ themes/custom/scaffold/src/Style/Patterns.php
@@ -24,28 +24,75 @@ class Patterns implements PatternProviderInterface {
   public function getPatterns() {
     $patterns = [];
     $patterns[] = $this->getStatusMessagePattern();
-    $patterns[] = $this->getHeaderPattern();
+    $patterns[] = $this->getTypeographyPattern();
+    $patterns[] = $this->getListsPattern();
+    $patterns[] = $this->getButtonsPattern();
     return $patterns;
   }
 
   /**
    * Pattern for <h> elements.
    */
-  private function getHeaderPattern() {
+  private function getTypeographyPattern() {
     $markup = '';
     foreach (range(1, 5) as $i) {
       $markup .= '<h' . $i . '>Header ' . $i . '</h' . $i . '>';
     }
-    return Pattern::atom('headers', 'Headers', [
+    $markup .= '<div><a href="#">Link</a></div>';
+    $markup .= '<div><p>This is paragraph text!</p></div>';
+    $markup .= '<div><blockquote>Blockquotes are the best. <cite>Abraham Lincoln</cite></blockquote></div>';
+    $markup .= '<div><hr /></div>';
+    return Pattern::atom('typography', 'Typography', [
       '#markup' => $markup,
     ]);
+  }
+
+  private function getListsPattern() {
+    return Pattern::atom('lists', 'Lists', [
+      'ordered' => [
+        '#theme' => 'item_list',
+        '#list_type' => 'ol',
+        '#items' => ['Item 1', 'Item 2', 'Item 3'],
+      ],
+      'unordered' => [
+        '#theme' => 'item_list',
+        '#items' => ['Item 1', 'Item 2', 'Item 3'],
+      ]
+    ]);
+  }
+
+  private function getButtonsPattern() {
+    $buttons = [];
+    $sizes = ['tiny', 'small', '', 'large', 'expanded', 'small expanded'];
+    foreach($sizes as $size) {
+      $buttons[] = [
+        '#type' => 'button',
+        '#value' => 'Button ' . $size,
+        '#attributes' => ['class' => [
+          'button',
+          $size
+        ]]
+      ];
+    }
+    $colors = ['secondary', 'success', 'alert', 'warning', 'disabled'];
+    foreach($colors as $color) {
+      $buttons[] = [
+        '#type' => 'button',
+        '#value' => 'Button ' . $color,
+        '#attributes' => ['class' => [
+          'button',
+          $color
+        ]]
+      ];
+    }
+    return Pattern::atom('buttons', 'Buttons', $buttons);
   }
 
   /**
    * Pattern for status messages.
    */
   private function getStatusMessagePattern() {
-    return Pattern::atom('alerts', 'Alerts', [
+    return Pattern::molecule('alerts', 'Alerts', [
       '#theme' => 'status_messages',
       '#message_list' => [
         'status' => ['Status Message'],
