3df3f3a9b6f5f99dbb9488e7e47b8542607fd75b
Rob Bayliss <rob@lastcallmedia.com>
2016-06-21 08:15:54 -0400

    Update compiled js

--- themes/custom/scaffold/dist/js/scaffold.js
+++ themes/custom/scaffold/dist/js/scaffold.js
@@ -3,18 +3,19 @@
  * Sitewide javascript behaviors.
  */
 (function ($) {
+  'use strict';
 
   /**
    * Attach and detach Foundation JS.
    */
   Drupal.behaviors.foundation = {
-    attach: function(context) {
+    attach: function (context) {
       $(context).foundation();
     },
-    detach: function(context) {
+    detach: function (context) {
       $(context).foundation('destroy');
     }
-  }
+  };
 
 })(jQuery);
 
