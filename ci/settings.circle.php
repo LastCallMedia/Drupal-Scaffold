5a2d2335eedc419a4c1e7e42c0d4810b35012453
Rob Bayliss <rob@lastcallmedia.com>
2017-01-09 18:01:38 -0500

    Add a circle build to mirror the travis one

---
+++ ci/settings.circle.php
@@ -0,0 +1,12 @@
+<?php
+
+$databases['default']['default'] = array(
+  'driver' => 'mysql',
+  'database' => 'circle_test',
+  'username' => 'ubuntu',
+  'password' => '',
+  'host' => '127.0.0.1',
+  'prefix' => '',
+);
+
+$settings['hash_salt'] = 'CIRCLE_CI';
