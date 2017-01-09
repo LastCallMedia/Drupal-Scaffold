6c39990a324dc7e6341d5666a65b2ec9a9d43cfa
Rob Bayliss <rob@lastcallmedia.com>
2017-01-09 15:49:01 -0500

    Add settings.*.php includes in default.settings.php and bring over to settings.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -86,7 +86,7 @@
  * );
  * @endcode
  */
- $databases = array();
+$databases = array();
 
 /**
  * Customizing database settings.
@@ -285,7 +285,7 @@ $config_directories = array();
  *   $settings['hash_salt'] = file_get_contents('/home/example/salt.txt');
  * @endcode
  */
-$settings['hash_salt'] = 'Lb_YCdFiDFP4sZHSTMQbuYQvRstCLREtJrR1ubcWocryvL9_NoFDIoRKV941QviA1hvRrYehUw';
+$settings['hash_salt'] = '';
 
 /**
  * Deployment identifier.
@@ -325,9 +325,6 @@ $settings['update_free_access'] = FALSE;
  *
  * You can also define an array of host names that can be accessed directly,
  * bypassing the proxy, in $settings['http_client_config']['proxy']['no'].
- *
- * If these settings are not configured, the system environment variables
- * HTTP_PROXY, HTTPS_PROXY, and NO_PROXY on the web server will be used instead.
  */
 # $settings['http_client_config']['proxy']['http'] = 'http://proxy_user:proxy_pass@example.com:8080';
 # $settings['http_client_config']['proxy']['https'] = 'http://proxy_user:proxy_pass@example.com:8080';
@@ -419,6 +416,20 @@ $settings['update_free_access'] = FALSE;
  * getting cached pages from the proxy.
  */
 # $settings['omit_vary_cookie'] = TRUE;
+
+
+/**
+ * Cache TTL for client error (4xx) responses.
+ *
+ * Items cached per-URL tend to result in a large number of cache items, and
+ * this can be problematic on 404 pages which by their nature are unbounded. A
+ * fixed TTL can be set for these items, defaulting to one hour, so that cache
+ * backends which do not support LRU can purge older entries. To disable caching
+ * of client error responses set the value to 0. Currently applies only to
+ * page_cache module.
+ */
+# $settings['cache_ttl_4xx'] = 3600;
+
 
 /**
  * Class Loader.
@@ -668,6 +679,15 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
 # $settings['container_base_class'] = '\Drupal\Core\DependencyInjection\Container';
 
 /**
+ * Override the default yaml parser class.
+ *
+ * Provide a fully qualified class name here if you would like to provide an
+ * alternate implementation YAML parser. The class must implement the
+ * \Drupal\Component\Serialization\SerializationInterface interface.
+ */
+# $settings['yaml_parser_class'] = NULL;
+
+/**
  * Trusted host configuration.
  *
  * Drupal core can use the Symfony trusted host mechanism to prevent HTTP Host
@@ -705,23 +725,31 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
  */
 
 /**
- * Load docker overrides, if available.
+ * The default list of directories that will be ignored by Drupal's file API.
+ *
+ * By default ignore node_modules and bower_components folders to avoid issues
+ * with common frontend tools and recursive scanning of directories looking for
+ * extensions.
+ *
+ * @see file_scan_directory()
+ * @see \Drupal\Core\Extension\ExtensionDiscovery::scanDirectory()
  */
-if(FALSE !== getenv('DOCKER_ENV')) {
-  require __DIR__ . '/settings.docker.php';
-}
+$settings['file_scan_ignore_directories'] = [
+  'node_modules',
+  'bower_components',
+];
 
 /**
- * Load local development override configuration, if available.
- *
- * Use settings.local.php to override variables on secondary (staging,
- * development, etc) installations of this site. Typically used to disable
- * caching, JavaScript/CSS compression, re-routing of outgoing emails, and
- * other things that should not happen on development and testing sites.
- *
- * Keep this code block at the end of this file to take full effect.
+ * Load environment-specific configuration overrides, if applicable.
  */
-if (file_exists(__DIR__ . '/settings.local.php')) {
-  include __DIR__ . '/settings.local.php';
+if(FALSE !== getenv('DOCKER_ENV') && file_exists(__DIR__ . '/settings.docker.php')) {
+  require __DIR__ . '/settings.docker.php';
 }
-$settings['install_profile'] = 'standard';
+// Always include a Pantheon settings file if one exists.
+if(file_exists(__DIR__ . '/settings.pantheon.php')) {
+  require __DIR__ . '/settings.pantheon.php';
+}
+// Include an optional local settings override.
+if(file_exists(__DIR__ . '/settings.local.php')) {
+  require __DIR__ . '/settings.local.php';
+}
