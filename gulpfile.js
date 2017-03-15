0f118d74acd2b3abbd2d70ebf8d56d683c87970c
Rob Bayliss <rob@lastcallmedia.com>
2017-03-15 11:55:26 -0400

    Fix path to gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@
   var opts = require('yargs').argv;
 
   var gulp = require('gulp-help')(require('gulp'));
-  var lcmGulpBuilder = require('./lcm-gulp-tasks');
+  var lcmGulpBuilder = require('lastcall-gulp-drupal-tasks');
 
   var config = require('./gulpconfig');
   lcmGulpBuilder(gulp, config, opts);
