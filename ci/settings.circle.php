4dfee86c2296c245e16faac30ba275c76172bf4c
Rob Bayliss <rob@lastcallmedia.com>
2017-08-31 07:41:12 -0400

    Do testing in the build step

--- ci/settings.circle.php
+++ ci/settings.circle.php
@@ -2,9 +2,9 @@
 
 $databases['default']['default'] = array(
   'driver' => 'mysql',
-  'database' => 'circle_test',
-  'username' => 'ubuntu',
-  'password' => '',
+  'database' => 'circle',
+  'username' => 'circle',
+  'password' => 'circle',
   'host' => '127.0.0.1',
   'prefix' => '',
 );
