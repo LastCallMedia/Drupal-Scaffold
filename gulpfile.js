b23a34c884a4c7fa76c5f94dbb164ab34f987be8
Rob Bayliss <rfbayliss@gmail.com>
2017-10-14 10:03:25 -0400

    Update Composer and NPM dependencies (#47)
    Drupal Core to 8.4.0
    Symfony components to 3.3.*
    Use legacy eslint configuration for now

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,10 @@
  */
 /* eslint-env node */
 
-(function () {
-  'use strict';
+// Load in CLI flags.
+var opts = require('yargs').argv;
+var gulp = require('gulp-help')(require('gulp'));
+var lcmGulpBuilder = require('lastcall-gulp-drupal-tasks');
+var config = require('./gulpconfig');
 
-  // Load in CLI flags.
-  var opts = require('yargs').argv;
-
-  var gulp = require('gulp-help')(require('gulp'));
-  var lcmGulpBuilder = require('lastcall-gulp-drupal-tasks');
-
-  var config = require('./gulpconfig');
-  lcmGulpBuilder(gulp, config, opts);
-
-})();
+lcmGulpBuilder(gulp, config, opts);
