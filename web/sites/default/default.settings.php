4cc80bb41dbe2cf9cc11f7bb68fb8e9ce022e22f
Rob Bayliss <rob@lastcallmedia.com>
2017-11-23 10:52:55 -0500

    Remove dockerized/local settings in default.settings.php

--- web/sites/default/default.settings.php
+++ web/sites/default/default.settings.php
@@ -768,18 +768,3 @@ $settings['file_scan_ignore_directories'] = [
  * larger number of entities to be processed in a single batch run.
  */
 $settings['entity_update_batch_size'] = 50;
-
-/**
- * Load environment-specific configuration overrides, if applicable.
- */
-if(FALSE !== getenv('DOCKER_ENV') && file_exists(__DIR__ . '/settings.docker.php')) {
-  require __DIR__ . '/settings.docker.php';
-}
-// Always include a Pantheon settings file if one exists.
-if(file_exists(__DIR__ . '/settings.pantheon.php')) {
-  require __DIR__ . '/settings.pantheon.php';
-}
-// Include an optional local settings override.
-if(file_exists(__DIR__ . '/settings.local.php')) {
-  require __DIR__ . '/settings.local.php';
-}
