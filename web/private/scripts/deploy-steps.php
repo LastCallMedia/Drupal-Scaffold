57b51a62d478513e0896c1b6e1cef2bf71434b90
Rob Bayliss <rob@lastcallmedia.com>
2017-12-06 19:06:12 -0500

    Add debug statements to deploy-steps

--- web/private/scripts/deploy-steps.php
+++ web/private/scripts/deploy-steps.php
@@ -3,4 +3,8 @@
  * @file
  * Quicksilver hook to execute deployment steps, as defined in bin/deploy-steps.
  */
+echo "Starting deployment steps";
+
 passthru('../bin/deploy-steps');
+
+echo "Deployment steps complete";
