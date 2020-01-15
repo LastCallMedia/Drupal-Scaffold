e7dad5d65ed651bba939938c6fb73fb8211618c7
Melissa Rossi <mrossi113@gmail.com>
2020-01-15 14:15:59 -0500

    comment out jQuery until needed for a project

--- backstop/scripts/ready.js
+++ backstop/scripts/ready.js
@@ -6,20 +6,20 @@
  * an element, you can probably deal with it here.
  */
 module.exports = async function(page, scenario, vp) {
-  await page.evaluate(function (url) {
-    if (!window.jQuery) {
-      throw new Error(`jQuery was not found. This is usually caused by the server returning a 500 response. Please check ${url} in your browser.`);
-    }
-    // Disable jQuery animation for any future calls.
-    jQuery.fx.off = true;
-    // Immediately complete any in-progress animations.
-    jQuery(':animated').finish();
-
-  }, scenario.url);
+  // await page.evaluate(function (url) {
+  //   if (!window.jQuery) {
+  //     throw new Error(`jQuery was not found. This is usually caused by the server returning a 500 response. Please check ${url} in your browser.`);
+  //   }
+  //   // Disable jQuery animation for any future calls.
+  //   jQuery.fx.off = true;
+  //   // Immediately complete any in-progress animations.
+  //   jQuery(':animated').finish();
+  //
+  // }, scenario.url);
   // Finally, wait for ajax to complete - this is to give alerts
   // time to finish rendering. This can take a while, especially
   // in local environments.
-  await page.waitForFunction('jQuery.active == 0');
+  // await page.waitForFunction('jQuery.active == 0');
 
   // Add a slight delay.  This covers up some of the jitter caused
   // by weird network conditions, slow javascript, etc. We should
